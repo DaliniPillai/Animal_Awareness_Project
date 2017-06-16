1b709e526363d29af2b580b25a0a1e93434b06a2
Dalini <student@GAs-MacBook-Air.local>
2017-06-16 15:03:23 -0400

    added animal_pic_url to table

--- db/seeds/animal.sql
+++ db/seeds/animal.sql
@@ -1,6 +1,6 @@
 \connect animal_awareness_development
 
-INSERT INTO animal (animal_type, animal_pic, animal_info) VALUES
+INSERT INTO animal (animal_type, animal_pic_url, animal_info) VALUES
   ('Cat','', ''),
   ('Dog','', ''),
   ('Parrot','', ''),
